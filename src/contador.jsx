19456232378d12248b90c6ea8a2ba41ff0fe73af
Brian Almada <brianalexisalmada84@gmail.com>
2024-01-07 00:40:29 -0300

    dividir en dos componentes el contador

--- src/contador.jsx
+++ src/contador.jsx
@@ -1,21 +1,42 @@
 import { useState } from 'react'
 
+const Display = (props) => {
+    return(
+        <div>{props.counter}</div>
+    )
+}
+
+const Button = (props) => {
+    return(
+        <button onClick={props.handleClick}>
+            {props.text}
+        </button>
+    )
+}
+
 const Contador = () => {
 
     const [counter, setCounter] = useState(0)
-    const counterPlus = () => {setCounter(counter + 1)}
-    const backToZero = () => {setCounter(0)}
+    const counterPlus = () => setCounter(counter + 1)
+    const counterDecreace = () => setCounter(counter - 1)
+
+    const backToZero = () => setCounter(0)
 
     return (
         <div>
-            <div>{counter}</div>
-            <button onClick={counterPlus}>
-                plus
-            </button>
-
-            <button onClick={backToZero}>
-                zero
-            </button>
+            <Display counter = {counter} />
+            <Button
+                handleClick={counterPlus}
+                text = 'plus'
+            />
+            <Button
+                handleClick={counterDecreace}
+                text = 'rest'
+            />
+            <Button
+                handleClick={backToZero}
+                text = 'zero'
+            />
         </div>
     )
 
