391c281b4f03afe801a9ffc477d022db9a7ac70b
Brian Almada <brianalexisalmada84@gmail.com>
2024-01-06 23:26:42 -0300

    definir controlador de contador dentro de una funcion

--- src/contador.jsx
+++ src/contador.jsx
@@ -1,14 +1,24 @@
-const App = () => {
+import { useState } from 'react'
+
+const Contador = () => {
 
     const [counter, setCounter] = useState(0)
+    const counterPlus = () => {setCounter(counter + 1)}
+    const backToZero = () => {setCounter(0)}
 
     return (
         <div>
             <div>{counter}</div>
-            <button onClick={() => setCounter(counter +1)}>
+            <button onClick={counterPlus}>
                 plus
+            </button>
+
+            <button onClick={backToZero}>
+                zero
             </button>
         </div>
     )
 
 }
+
+export default Contador
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import Contador from './contador.jsx'
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+ReactDOM.createRoot(document.getElementById('root')).render(<Contador />)
