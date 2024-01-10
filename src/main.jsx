113c56a1efce8eb689bf2bad4091893cc7783348
Brian Almada <brianalexisalmada84@gmail.com>
2024-01-09 23:54:09 -0300

    Armar estructura para practicar callbacks

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import Contador from './contador.jsx'
 import Clicker from './Clicks.jsx'
+import Callbacks from './callbacks.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Clicker />)
+ReactDOM.createRoot(document.getElementById('root')).render(<Callbacks />)
