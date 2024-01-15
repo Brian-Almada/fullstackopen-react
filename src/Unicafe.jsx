57d319e4089d7ad3cf7cdcfb90cab258e3b3d98d
Brian Almada <brianalexisalmada84@gmail.com>
2024-01-15 17:17:12 -0300

    Refactorizar agregando componente Button y StatisticsLine

--- src/Unicafe.jsx
+++ src/Unicafe.jsx
@@ -1,5 +1,22 @@
 import { useState } from "react";
 
+const Button = ({handleClick, text}) => <button onClick={handleClick}>{text}</button>
+
+const StatisticsLine = ({text, value}) => {
+    <p>{text} {value}</p>
+
+    return (
+        <>
+            <StatisticsLine text="good" value={good} />
+            <StatisticsLine text="neutral" value={neutral}/>
+            <StatisticsLine text="bad" value={bad} />
+            <StatisticsLine text="all" value={all} />
+            <StatisticsLine text="average" value={average} />
+            <StatisticsLine text="positive" value={`${positive} %`} />
+        </>
+    )
+}
+
 const Statistics = ({good, neutral, bad}) => {
 
     const all = good + neutral + bad
@@ -17,6 +34,7 @@ const Statistics = ({good, neutral, bad}) => {
         return (
             <>
             <h1>statistics</h1>
+            <h1>statistics</h1>
             <p>good {good}</p>
             <p>neutral {neutral}</p>
             <p>bad {bad}</p>
@@ -28,6 +46,8 @@ const Statistics = ({good, neutral, bad}) => {
     }
 
 }
+
+
 const Unicafe = () => {
 
     const [good, setGood] = useState(0)
@@ -39,13 +59,12 @@ const Unicafe = () => {
     return (
         <>
         <h1>give feedback</h1>
-        <button onClick={() => setGood(good + 1)}>good</button>
-        <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
-        <button onClick={() => setBad (bad + 1)}>bad</button>
+        <Button handleClick={() => setGood(good + 1)} text= 'good' />
+        <Button handleClick={() => setNeutral(neutral + 1)} text= 'neutral' />
+        <Button handleClick={() => setBad (bad + 1)} text= 'bad' />
 
         <Statistics good={good} neutral={neutral} bad={bad} />
         </>
-
     )
 
 }
