b4bb2acfce825b9d05b95fa96cbaf8897369be7c
Brian Almada <brianalexisalmada84@gmail.com>
2024-01-17 22:58:42 -0300

    Crear ejercicio para remderizar aleatoreamente los elementos de un array

--- src/main.jsx
+++ src/main.jsx
@@ -4,5 +4,6 @@ import Contador from './contador.jsx'
 import Clicker from './Clicks.jsx'
 import Callbacks from './callbacks.jsx'
 import Unicafe from './Unicafe.jsx'
+import Work from './Work.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<Unicafe />)
+ReactDOM.createRoot(document.getElementById('root')).render(<Work />)
