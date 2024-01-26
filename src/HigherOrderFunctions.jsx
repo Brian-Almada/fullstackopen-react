f74c9d8f6cbfed530831a4decf756f0f64faaa92
Brian Almada <brianalexisalmada84@gmail.com>
2024-01-25 23:00:05 -0300

    Practicar las funciones de orden superior usando

--- src/HigherOrderFunctions.jsx
+++ src/HigherOrderFunctions.jsx
@@ -38,3 +38,20 @@ for (let i = 0; i < animals.length; i++) {
 }
 */
 console.log(names)
+//-----------------------------------------------------------------//
+
+// Higher order functions => Reduce //
+
+let orders = [
+    { amount: 250 },
+    { amount: 400 },
+    { amount: 100 },
+    { amount: 325 }
+]
+
+let totalAmount = 0
+for(let i = 0; i < orders.length; i++) {
+    totalAmount += orders[i].amount
+}
+
+console.log(totalAmount)
