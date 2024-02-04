c7e1812150170174c3911ee486df9152b22343ed
Brian Almada <brianalexisalmada84@gmail.com>
2024-02-04 01:15:01 -0300

    Corregir errores de logica

--- src/Notations.jsx
+++ src/Notations.jsx
@@ -1,41 +1,20 @@
 import React from 'react'
 import Note from './components/Note'
 
-const notes = [
-    {
-        id: 1,
-        content: 'HTML in easy',
-        date: '2019-05-30T17:30:31.098Z',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Browser can execute only JavaScript',
-        date: '2019-05-30T18:39:34.091Z',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'GET and POST are the most important methods of HTTP protocol',
-        date: '2019-05-30T19:20:14.298Z',
-        important: true
-    }
-]
 
-const Notex = ({ notes }) => {
+const Notex = ({notes}) => {
 
     return (
         <div>
             <h1>Notes</h1>
             <ul>
-                {notes.map(note => (
+                {notes.map(note =>
                     <Note key={note.id} note={note}/>
-                ))}
+                )}
             </ul>
         </div>
     )
 }
 
-console.log(notes)
 export default Notex
 //para renderizar hay que cambiar la ruta de src en index.html
