c984b48e8f15800c65649554678a756aa532252c
Brian Almada <brianalexisalmada84@gmail.com>
2024-02-09 23:56:06 -0300

    Renderizar todos los cursos y el total de los cursos

--- src/components/Course.jsx
+++ src/components/Course.jsx
@@ -1,42 +1,11 @@
-
-const Header = () => {
-
-    return (
-        <div>
-            <h1>Web development curriculum</h1>
-        </div>
-    )
-}
-
-const Part = ({part}) => {
-    return (
-        <div>
-            <p>{part.name} {part.exercises}</p>
-        </div>
-    )
-}
-
-const Content = ({parts}) => {
-    console.log(parts)
-    const totalCourse = parts.reduce((total, part) => total + part.exercises, 0)
-    return (
-        <>
-            {
-                parts.map((part) => {
-                    <Part key={part.id} part={part}/>
-                })
-            }
-            <b>total of {totalCourse} exercises</b>
-        </>
-    )
-}
-
+import Header from "./Header"
+import Content from "./Content"
 
 const Course = ({course}) => {
     return (
         <div>
-            <Header />
-            <Content parts={course.parts} />
+            <Header course={course.name}/>
+            <Content parts={course.parts}/>
         </div>
     )
 }
