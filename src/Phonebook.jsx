d7bf88f3916dfe704377cfb7f0da5ad4d859876d
Brian Almada <brianalexisalmada84@gmail.com>
2024-02-19 18:48:25 -0300

    Agregar manejador de eventos para el input

--- src/Phonebook.jsx
+++ src/Phonebook.jsx
@@ -6,18 +6,28 @@ const Phonebook = () => {
     ])
     const [newName, setNewName] = useState('')
 
+    const handleInputChange = (e) => {
+        e.preventDefault()
+        setNewName(e.target.value)
+    }
+
     return (
         <div>
             <h2>Phonebook</h2>
             <form>
                 <div>
-                    name: <input/>
+                    name:
+                    <input
+                        value={newName}
+                        onChange={handleInputChange}
+                    />
                 </div>
                 <div>
                     <button type="submit">add</button>
                 </div>
             </form>
             <h2>Numbers</h2>
+            <div>debug: {newName}</div>
         </div>
     )
 }
