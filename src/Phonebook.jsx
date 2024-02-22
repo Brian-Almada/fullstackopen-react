33c83e68656e9fbaabd690a92a283bdbd1c3546e
Brian Almada <brianalexisalmada84@gmail.com>
2024-02-21 22:02:18 -0300

    Agregar condicional para no agregar nombres repetidos al arreglo

--- src/Phonebook.jsx
+++ src/Phonebook.jsx
@@ -12,7 +12,11 @@ const Phonebook = () => {
 
     const addPerson = (e) => {
         e.preventDefault()
-        setPersons(persons.concat({name: newName}))
+        if (persons.some(person => person.name === newName)) {
+            alert(`${newName} is already added to phonebook`)
+        } else {
+            setPersons(persons.concat({name: newName}))
+        }
     }
 
     const handleInputChange = (e) => {
