ff451a33069d7b6ea60420b3dece52c9e06ba1bf
Brian Almada <brianalexisalmada84@gmail.com>
2024-02-27 23:35:43 -0300

    Crear archivo

--- src/main.jsx
+++ src/main.jsx
@@ -8,15 +8,5 @@ import Work from './Work.jsx'
 import Listado from './Listado.jsx'
 import NoteApp from './NoteApp.jsx'
 import Phonebook from './Phonebook.jsx'
-import axios from 'axios'
-
-const promise = axios.get('http://localhost:3001/notes')
-promise.then(response => {
-    console.log(response)  
-})
-console.log(promise)
-
-const promise2 = axios.get('http://localhost:3001/foo nbar')
-console.log(promise2)
 
 ReactDOM.createRoot(document.getElementById('root')).render(<Phonebook />)
